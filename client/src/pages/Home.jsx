d254b829897d75db7b5166291d8143e5ca3c3018
Your GitHub Username <Your GitHub Email>
2024-03-30 14:21:17 +0200

    setup store index and pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,43 @@
 import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+import {
+  headContentAnimation,
+  headContainerAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 export default function Home() {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="ThreeJS Logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className=" text-5xl font-extrabold text-center">
+                LET&apos;S <br className="xl:block hidden" /> DO IT.
+              </h1>
+            </motion.div>
+            <motion.div>
+              <p>
+                Create your unique and exclusive shirt with our brand-new
+                customization tool.<strong>unleash your imagination.</strong>{" "}
+                and define your own style.
+              </p>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
 }
