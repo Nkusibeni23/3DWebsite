b5f4c00de7990a9ecf9f62243672c1ffc791f646
Your GitHub Username <Your GitHub Email>
2024-03-31 19:41:05 +0200

    tab icons

--- client/src/components/Tab.jsx
+++ client/src/components/Tab.jsx
@@ -1,3 +1,25 @@
-export default function Tab() {
-  return <div>Tab</div>;
-}
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const Tab = ({ tab, isFilterTab, isActiveTab, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const activeStyles =
+    isFilterTab && isActiveTab
+      ? { backgroundColor: snap.color, opacity: 0.5 }
+      : { backgroundColor: "transparent", opacity: 1 };
+
+  return (
+    <div
+      key={tab.name}
+      className={`tab-btn ${
+        isFilterTab ? "rounded-full bg-black" : "rounded-4"
+      }`}
+      onClick={handleClick}
+    >
+      <img src={tab.icon} alt="Icon" />
+    </div>
+  );
+};
+export default Tab;
